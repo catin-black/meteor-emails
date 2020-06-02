c14fac08fc5a24849e548214572ccbd1b71b0271
Adam Trojańczyk <adam.trojanczyk@gmail.com>
2020-06-02 13:49:08 +0200

    Removing method from cron

--- server/cron.js
+++ server/cron.js
@@ -1,23 +1,23 @@
 SyncedCron.config({
-	// Log job run details to console
-	log: true,
-	// Use a custom logger function (defaults to Meteor's logging package)
-	logger: null,
-	// Name of collection to use for synchronisation and logging
-	collectionName: 'cronHistory',
-	// Default to using localTime
-	utc: false,
-	/*
-		TTL in seconds for history records in collection to expire
-		NOTE: Unset to remove expiry but ensure you remove the index from
-		mongo by hand
-		ALSO: SyncedCron can't use the `_ensureIndex` command to modify
-		the TTL index. The best way to modify the default value of
-		`collectionTTL` is to remove the index by hand (in the mongo shell
-		run `db.cronHistory.dropIndex({startedAt: 1})`) and re-run your
-		project. SyncedCron will recreate the index with the updated TTL.
-	*/
-	collectionTTL: 172800
+    // Log job run details to console
+    log: true,
+    // Use a custom logger function (defaults to Meteor's logging package)
+    logger: null,
+    // Name of collection to use for synchronisation and logging
+    collectionName: 'cronHistory',
+    // Default to using localTime
+    utc: false,
+    /*
+    	TTL in seconds for history records in collection to expire
+    	NOTE: Unset to remove expiry but ensure you remove the index from
+    	mongo by hand
+    	ALSO: SyncedCron can't use the `_ensureIndex` command to modify
+    	the TTL index. The best way to modify the default value of
+    	`collectionTTL` is to remove the index by hand (in the mongo shell
+    	run `db.cronHistory.dropIndex({startedAt: 1})`) and re-run your
+    	project. SyncedCron will recreate the index with the updated TTL.
+    */
+    collectionTTL: 172800
 });
 
 
@@ -27,7 +27,8 @@ SyncedCron.add({
         return parser.text('every 30 minutes');
     },
     job: function() {
-       Meteor.call('stats');
+        // TODO: Get stats for all users
+    	return;
     }
 });
 
